1111ce625c493aac42b3acf5cba73bd505bb14da
Hüseyin Demir <huseyin.demir@C02W22S7HV2R.local>
2018-10-15 11:16:06 +0300

    Version 3.1.2 - Dbatools Function Deployment

--- SqlDbaManager/SqlDbaManager.psm1
+++ SqlDbaManager/SqlDbaManager.psm1
@@ -227,3 +227,44 @@ function Install-PostSql {
     }
 }
 
+function Install-dbatoolsBestPractice {
+    [CmdletBinding()]
+    param 
+    (
+        
+        
+    )
+    
+    begin 
+    {
+        $serverName = "localhost"
+    }
+    
+    process {
+
+        try {
+            #Install dbatools
+             $env:PSModulePath=[Environment]::GetEnvironmentVariable("PSModulePath", "Machine")
+    
+            if (Get-Module -ListAvailable -Name dbatools) {Write-Output "dbatools module exists."} 
+            else {Write-Output "dbatools module does not exist."
+                Invoke-Expression (Invoke-WebRequest -UseBasicParsing https://dbatools.io/in)}
+    
+            #set dbatools best practice
+            Write-Host "# Started dbatools"
+            Set-DbaPowerPlan -ComputerName $serverName
+            Write-Host "Powerplan Configuration was Done" -ForegroundColor Green -BackgroundColor Black
+            Set-DbaMaxDop -SqlServer $serverName -MaxDop 4
+            Write-Host "MaxDop Configuration was Done" -ForegroundColor Green -BackgroundColor Black
+            Set-DbaMaxMemory -SqlServer $serverName
+            Write-Host "MaxMemory Configuration was Done" -ForegroundColor Green -BackgroundColor Black
+            Write-Host "# Finished dbatools"
+            
+        }
+        catch {
+            $ErrorMsg = $_.Exception.Message 
+            return "# FAILED - SqlConfig: $ErrorMsg"
+        }
+    }
+}
+
