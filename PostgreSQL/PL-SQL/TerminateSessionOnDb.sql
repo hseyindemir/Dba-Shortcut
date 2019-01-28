d7a82df7e010c126b3381ea6a8b6070b1d3c0fa0
hseyindemir <demirhuseyinn.94@gmail.com>
2019-01-28 22:36:34 +0300

    Idle In Transaction Killer Script Added.

--- PostgreSQL/PL-SQL/TerminateSessionOnDb.sql
+++ PostgreSQL/PL-SQL/TerminateSessionOnDb.sql
@@ -1,5 +1,12 @@
 ------------------------- Idle Durumdaki Tüm Sorguları Kill Eder--------------------------------------
 
 select pg_terminate_backend(pid) from pg_stat_activity where state = 'idle';
+------------------------------------------------------------------------------------------------------
+
+
+
+------------------------- Idle in Transaction Durumundaki Tüm Sorguları Kill Eder---------------------
+
+select pg_terminate_backend(pid) from pg_stat_activity where state = 'idle in transaction';
 
 ------------------------------------------------------------------------------------------------------
